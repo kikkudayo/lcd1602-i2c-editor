3f8b82ad1fc187955ed62915405aab7199326cb4
kikkudayo <gabisaac2000@gmail.com>
2026-03-14 23:13:09 +0800

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/REPOSITORY-NAME/",
+  base: "/lcd1602-i2c-editor/",
 })
